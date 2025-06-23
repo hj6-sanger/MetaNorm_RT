502a82a5f80e04339bd4848e50112902cc526a92
hj6-sanger <94840292+hj6-sanger@users.noreply.github.com>
2025-06-23 11:24:49 +0100

    Update Landscape.R

--- Figure_1/Landscape.R
+++ Figure_1/Landscape.R
@@ -26,6 +26,11 @@ ggplot(t1, aes(x=type, y=mean)) + geom_pointrange(aes(ymin=mean-std, ymax=mean+s
 #we used a negative binomial mixed-effects model with a binary indicator variable (case and control group). 
 #The group indicator variable (case and control) and donor ID were set as fixed and random effects, respectively, while adjusting for age and sequencing coverage. 
 #The fold-change of retrotransposition counts for each comparison was estimated by exponentiating the coefficient of the indicator variable.
+#"group_var" indicates whether each entry is a case or a control and the fold-change was computed by exponentiating the coefficient of "group_var". 
+
+t1=read.table('2NUM_FINAL_tissue-sample-FAP.txt',sep='\t',header=TRUE)
+lmm.sbs.age <- glm.nb(count~ age+group_var +cov+ (1|donor), data = t1)
+summary(lmm.sbs.age)
 
 
 
