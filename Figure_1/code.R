3cfaa0fbaa153baa4377ecf8641412f6af7cdfac
hj6-sanger <94840292+hj6-sanger@users.noreply.github.com>
2025-06-23 13:01:46 +0100

    Update code.R

--- Figure_1/code.R
+++ Figure_1/code.R
@@ -18,7 +18,7 @@ t1=read.table('age_coverage.txt',sep='\t',header=TRUE)
 lmm.sbs.age <- glm.nb(count~ age+cov + (1|donor), data = t1)
 summary(lmm.sbs.age)
 
-#R code to generate Figure 1c
+#R code to generate Figure 1c and 3a
 ggplot(t1, aes(x=type, y=mean)) + geom_pointrange(aes(ymin=mean-std, ymax=mean+std)) + scale_y_log10()  
 + theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
@@ -31,6 +31,14 @@ ggplot(t1, aes(x=type, y=mean)) + geom_pointrange(aes(ymin=mean-std, ymax=mean+s
 t1=read.table('2NUM_FINAL_tissue-sample-FAP.txt',sep='\t',header=TRUE)
 lmm.sbs.age <- glm.nb(count~ age+group_var +cov+ (1|donor), data = t1)
 summary(lmm.sbs.age)
+
+
+#R code to generate the fraction of full-length L1 insertion
+ggplot(t1, aes(fill=factor(type), y=y,x=tissue)) + 
+geom_bar(position="fill", stat="identity") +scale_fill_manual(values=c("#6699CC","#f45763")) + theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+
+
 
 
 
