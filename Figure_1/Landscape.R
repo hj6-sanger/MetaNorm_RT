b94735d3a2c70f5099e9f988a9a5ebcac118a5ce
hj6-sanger <94840292+hj6-sanger@users.noreply.github.com>
2025-06-23 10:59:32 +0100

    Update Landscape.R

--- Figure_1/Landscape.R
+++ Figure_1/Landscape.R
@@ -1,7 +1,7 @@
 
 #R code to generate Figure 1
-#Input is a text file with three columns, where the columns "X" and "Y" represent tissue type and the number of samples affected according to the 
-count category (the column "type").
+#Input is a text file with three columns, where the columns "X" and "Y" represent tissue type and the number of samples affected 
+#according to the count category (the column "type").
 t1=read.table("input.txt",header=TRUE)
 ggplot(t1, aes(fill=factor(type), y=Y, x=X)) + geom_bar(position="fill", stat="identity")+
 scale_fill_manual(values=c("#CCCCCC","#FFCC00","#FF6633", "#CC3333","#660033"))+theme_bw() + 
