6d392fd273aa6795a9db6c6642ac5568197ec7b4
hj6-sanger <94840292+hj6-sanger@users.noreply.github.com>
2025-06-23 11:21:08 +0100

    Update Landscape.R

--- Figure_1/Landscape.R
+++ Figure_1/Landscape.R
@@ -1,5 +1,5 @@
 
-#R code to generate Figure 1
+#R code to generate Figure 1a
 #Input is a text file with three columns, where the columns "X" and "Y" represent tissue type and the number of samples affected 
 #according to the count category (the column "type"),repectively. 
 t1=read.table("input.txt",header=TRUE)
@@ -7,7 +7,7 @@ ggplot(t1, aes(fill=factor(type), y=Y, x=X)) + geom_bar(position="fill", stat="i
 scale_fill_manual(values=c("#CCCCCC","#FFCC00","#FF6633", "#CC3333","#660033"))+theme_bw() + 
 theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
-#R code to generate Figure 2
+#R code to generate Figure 1b
 #Input is a text file with three columns, where the columns "age" and "count" represent samples' age and sample' count, respectively. 
 ggplot(t1, aes(x=age, y=count)) + geom_point() + geom_smooth(method=lm) +scale_y_log10()  +
 theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
@@ -17,4 +17,15 @@ theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = elemen
 t1=read.table('age_coverage.txt',sep='\t',header=TRUE)
 lmm.sbs.age <- glm.nb(count~ age+cov + (1|donor), data = t1)
 summary(lmm.sbs.age)
+
+#R code to generate Figure 1c
+ggplot(t1, aes(x=type, y=mean)) + geom_pointrange(aes(ymin=mean-std, ymax=mean+std)) + scale_y_log10()  
++ theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+#R code to compare the retrotransposition counts between case and control,
+#we used a negative binomial mixed-effects model with a binary indicator variable (case and control group). 
+#The group indicator variable (case and control) and donor ID were set as fixed and random effects, respectively, while adjusting for age and sequencing coverage. 
+#The fold-change of retrotransposition counts for each comparison was estimated by exponentiating the coefficient of the indicator variable.
+
+
 
